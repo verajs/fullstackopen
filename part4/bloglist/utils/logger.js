8824b2e91eb245a6581f2244b92c5a4937598549
Sebastian Vera <100543895+verajs@users.noreply.github.com>
2022-06-26 01:36:29 -0400

    Testing the backend and implementing user authentication

---
+++ part4/bloglist/utils/logger.js
@@ -0,0 +1,22 @@
+const info = (...params) => {
+  if (process.env.NODE_ENV !== "test") {
+    console.log(...params);
+  }
+};
+
+const error = (...params) => {
+  if (process.env.NODE_ENV !== "test") {
+    console.error(...params);
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({
+      error: error.message,
+    });
+  } else if (error.name === "JsonWebTokenError") {
+    return response.status(401).json({ error: "invalid token" });
+  }
+};
+
+module.exports = {
+  info,
+  error,
+};
